2913da8c27372c6809c51bd4923154dac19f01db
Jcpro <jcproductions.my@gmail.com>
2021-02-02 04:24:02 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import QuestionLists from './components/QuestionLists'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL =
+  'https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple'
+
+const App = () => {
+  const [questions, setQuestions] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [score, setScore] = useState(0)
+  const [showCorrectAnswer, setShowCorrectAnswer] = useState(false)
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        const questions = data.results.map((questions) => ({
+          ...questions,
+          answers: [
+            questions.correct_answer,
+            ...questions.incorrect_answers,
+          ].sort(() => Math.random() - 0.5),
+        }))
+        setQuestions(questions)
+      })
+  }, [])
+
+  const handleAnswer = (answer) => {
+    setShowCorrectAnswer(true)
+    if (answer === questions[currentIndex].correct_answer) {
+      setScore(score + 1)
+    }
+  }
+
+  const handleNext = () => {
+    setCurrentIndex(currentIndex + 1)
+    setShowCorrectAnswer(false)
+  }
+
+  return questions.length !== 0 ? (
+    questions.length > currentIndex ? (
+      <div style={{ background: 'purple' }}>
+        <QuestionLists
+          data={questions[currentIndex]}
+          showCorrectAnswer={showCorrectAnswer}
+          handleNext={handleNext}
+          handleAnswer={handleAnswer}
+        />
+      </div>
+    ) : (
+      <>
+        <span>Your score: {score}</span> <br />
+        <button onClick={() => setCurrentIndex(0)}>Try again ? </button>
+      </>
+    )
+  ) : (
+    'Loading...'
+  )
 }
 
-export default App;
+export default App
